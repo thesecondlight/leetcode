049313cd5ea62b1e30f54ce78eae12bfc10c6e3d
thesecondlight <1092876640@qq.com>
2019-09-29 10:37:21 +0800

    昨日成就更改

--- code/remove-element.js
+++ code/remove-element.js
@@ -1,10 +1,11 @@
 var removeElement = function (nums, val) {
+  let k=0
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === val) {
-      nums.splice(i, 1)
-      i--
+    if (nums[i] !== val) {
+      nums[k]=nums[i]
+      k++
     }
   }
-  return nums.length
+  return k
 }
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
