aad5a68ca674f0f852004fca33c2302dcfa5d789
thesecondlight <1092876640@qq.com>
2019-10-11 14:10:16 +0800

    achievement 1010

---
+++ code/sqrtx.js
@@ -0,0 +1,14 @@
+var mySqrt = function(x) {
+    let temp=x;
+    while(temp*temp>x){
+        const next=(temp+x/temp)/2
+        if(next===temp){
+            break;
+        }
+        else{
+            temp=next;
+        }
+    }
+    return Math.floor(temp)
+};
+console.log(mySqrt(10))
