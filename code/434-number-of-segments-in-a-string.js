8f2d9dd03b8c55711483266e9d91ce8115f8e003
thesecondlight <1092876640@qq.com>
2019-11-06 21:03:21 +0800

    123

--- code/434-number-of-segments-in-a-string.js
+++ code/434-number-of-segments-in-a-string.js
@@ -1,13 +1,12 @@
-var countSegments =function(s){
-    s+=' '
-    let count=0
-    for(let i=0;i<s.length-1;i++){
-        if(s[i] != s[i+1] && s[i+1] == ' '){
-            count++;
-        }
+var countSegments = function (s) {
+  s += ' '
+  let count = 0
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] !== s[i + 1] && s[i + 1] === ' ') {
+      count++
     }
-    
-    return count
-}
-console.log(countSegments("Hello, my name is cheng Jiang"))
+  }
 
+  return count
+}
+console.log(countSegments('Hello, my name is cheng Jiang'))
